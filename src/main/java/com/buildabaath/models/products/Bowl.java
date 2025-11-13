a88f658ce22da72d9b95d659527c29cc17027385
AJ V <avaithyam@yahoo.com>
2025-11-13 03:11:46 -0500

    the grind continues

--- src/main/java/com/buildabaath/models/products/Bowl.java
+++ src/main/java/com/buildabaath/models/products/Bowl.java
@@ -2,10 +2,11 @@ package com.buildabaath.models.products;
 
 import com.buildabaath.interfaces.Customizable;
 import com.buildabaath.models.abstracts.Item;
+import com.buildabaath.models.properties.MainItemType;
 import com.buildabaath.models.toppings.Protein;
 
 public class Bowl extends Item implements Customizable {
-    private MainType type;
+    private MainItemType type;
     private String size;
     private Protein protein;
     private
